811a0feb90b14bd710fca92f8602bba2428900b5
Vincent Moore <devdebonair@gmail.com>
2017-01-04 12:59:50 -0700

    not sure what happened

--- snoogle/CellNodeMedia.swift
+++ snoogle/CellNodeMedia.swift
@@ -12,21 +12,87 @@ import AsyncDisplayKit
 
 class CellNodeMedia: ASCellNode {
     
-    let photo: ASNetworkImageNode
+    let media: MediaElement
+    var mediaView = ASImageNode()
+    let inset: UIEdgeInsets
     
-    init(url: URL?) {
-        self.photo = ASNetworkImageNode()
-        self.photo.url = url
-        self.photo.contentMode = .scaleAspectFill
+    init(media: MediaElement, inset: UIEdgeInsets = .zero) {
+        self.media = media
+        self.inset = inset
         
         super.init()
         
         automaticallyManagesSubnodes = true
+        
+        if let media = media as? Photo {
+            if let _ = media.urlSmall, let _ = media.urlMedium, let _ = media.urlLarge {
+                mediaView = ASMultiplexImageNode()
+                if let mediaView = mediaView as? ASMultiplexImageNode {
+                    mediaView.imageIdentifiers = ["large" as NSCopying & NSObjectProtocol, "medium" as NSCopying & NSObjectProtocol, "small" as NSCopying & NSObjectProtocol]
+                    mediaView.downloadsIntermediateImages = true
+                    mediaView.dataSource = self
+                    mediaView.isLayerBacked = true
+                }
+            } else {
+                mediaView = ASNetworkImageNode()
+                if let mediaView = mediaView as? ASNetworkImageNode {
+                    mediaView.url = media.url
+                    mediaView.contentMode = .scaleAspectFill
+                    mediaView.isLayerBacked = true
+                }
+            }
+        }
+        
+        if let media = media as? Video {
+            mediaView = ASVideoNode()
+            if let mediaView = mediaView as? ASVideoNode {
+                mediaView.url = media.poster
+                mediaView.gravity = AVLayerVideoGravityResizeAspectFill
+                mediaView.shouldAutoplay = true
+                mediaView.shouldAutorepeat = true
+                mediaView.placeholderEnabled = true
+                mediaView.placeholderFadeDuration = 2.0
+                mediaView.backgroundColor = .black
+                mediaView.muted = true
+                mediaView.isLayerBacked = true
+            }
+        }
+    }
+    
+    override func didEnterVisibleState() {
+        if let media = media as? Video, let mediaView = mediaView as? ASVideoNode {
+            DispatchQueue.global(qos: .background).async {
+                if let url = media.url {
+                    let asset = AVAsset(url: url)
+                    DispatchQueue.main.async {
+                        mediaView.asset = asset
+                    }
+                }
+            }
+        }
     }
     
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        let ratioSpec = ASRatioLayoutSpec(ratio: 1.0, child: photo)
-        return ratioSpec
+        let ratio = CGFloat(media.height/media.width)
+        let ratioSpec = ASRatioLayoutSpec(ratio: ratio, child: mediaView)
+        let insetSpec = ASInsetLayoutSpec(insets: inset, child: ratioSpec)
+        return insetSpec
     }
     
+}
+
+extension CellNodeMedia: ASMultiplexImageNodeDataSource {
+    func multiplexImageNode(_ imageNode: ASMultiplexImageNode, urlForImageIdentifier imageIdentifier: ASImageIdentifier) -> URL? {
+        guard let media = media as? Photo, let imageIdentifier = imageIdentifier as? String else { return nil }
+        switch imageIdentifier {
+        case "small":
+            return media.urlSmall
+        case "medium":
+            return media.urlMedium
+        case "large":
+            return media.urlLarge
+        default:
+            return nil
+        }
+    }
 }
--- snoogle/CellNodeText.swift
+++ snoogle/CellNodeText.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+import AsyncDisplayKit
+
+class CellNodeText: ASCellNode {
+    
+    let textNode = ASTextNode()
+    let inset: UIEdgeInsets
+    
+    init(attributedText: NSMutableAttributedString, inset: UIEdgeInsets = .zero) {
+        self.inset = inset
+        
+        super.init()
+        
+        textNode.attributedText = attributedText
+        textNode.maximumNumberOfLines = 0
+        textNode.isLayerBacked = true
+        
+        automaticallyManagesSubnodes = true
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        return ASInsetLayoutSpec(insets: inset, child: textNode)
+    }
+    
+}
