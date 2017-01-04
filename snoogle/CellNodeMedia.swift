fefb72c63ecf042728bd6cd277da2b689e17cadc
Vincent Moore <devdebonair@gmail.com>
2017-01-04 12:59:10 -0700

    added cellnodemedia and cellnodetext

---
+++ snoogle/CellNodeMedia.swift
@@ -0,0 +1,32 @@
+//
+//  CellNodePhoto.swift
+//  snoogle
+//
+//  Created by Vincent Moore on 12/28/16.
+//  Copyright © 2016 Vincent Moore. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AsyncDisplayKit
+
+class CellNodeMedia: ASCellNode {
+    
+    let photo: ASNetworkImageNode
+    
+    init(url: URL?) {
+        self.photo = ASNetworkImageNode()
+        self.photo.url = url
+        self.photo.contentMode = .scaleAspectFill
+        
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let ratioSpec = ASRatioLayoutSpec(ratio: 1.0, child: photo)
+        return ratioSpec
+    }
+    
+}
---
+++ snoogle/CellNodeText.swift
@@ -0,0 +1,9 @@
+//
+//  CellNodeText.swift
+//  snoogle
+//
+//  Created by Vincent Moore on 1/3/17.
+//  Copyright © 2017 Vincent Moore. All rights reserved.
+//
+
+import Foundation
