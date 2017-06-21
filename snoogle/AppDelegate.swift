ec112b9682f35f8f1a9773509d6c5c145f848d6b
Vincent Moore <devdebonair@gmail.com>
2017-06-21 10:07:25 -0400

    moved markdown into libraries group

--- snoogle/AppDelegate.swift
+++ snoogle/AppDelegate.swift
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print(error)
         }
         
-        let rootController = FeedCollectionController(name: "rocketleague")
+        let rootController = FeedCollectionController(name: "incest")
         navigationController = ASNavigationController(rootViewController: rootController)
         
         window = UIWindow(frame: UIScreen.main.bounds)
