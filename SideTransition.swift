3573638142848eefac15bed17481afd64f7bfd37
Vincent Moore <devdebonair@gmail.com>
2017-07-01 16:03:12 -0400

    fixed breaking links for libraries

---
+++ SideTransition.swift
@@ -0,0 +1,169 @@
+//
+//  SideTransition.swift
+//  snoogle
+//
+//  Created by Vincent Moore on 6/24/17.
+//  Copyright © 2017 Vincent Moore. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AsyncDisplayKit
+
+class SlideTransition: Transition {
+    
+    var isInteractive: Bool = false
+    var menuController: UIViewController? = nil
+    var mainController: UIViewController? = nil {
+        didSet {
+            guard let mainController = mainController else { return }
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(interactionPanHandlerForPresentation(pan:)))
+            mainController.view.addGestureRecognizer(panGesture)
+        }
+    }
+    
+    private let overlayNode = ASDisplayNode()
+    private var snapshot: UIView!
+
+    private var offsetMenuModifier: CGFloat = 0.10
+    private var offsetMainModifier: CGFloat = 0.10
+    
+    override func present(toController: UIViewController, fromController: UIViewController, container: UIView, completion: @escaping (Bool) -> Void) {
+        
+        snapshot = fromController.view.snapshotView(afterScreenUpdates: true)
+        
+        if !isInteractive {            
+            let pan = UIPanGestureRecognizer(target: self, action: #selector(interactionPanHandler(pan:)))
+            let tap = UITapGestureRecognizer(target: self, action: #selector(didTapOverlay))
+            snapshot.addGestureRecognizer(pan)
+            snapshot.addGestureRecognizer(tap)
+        }
+        
+        overlayNode.backgroundColor = .white
+        overlayNode.frame = fromController.view.frame
+        overlayNode.clipsToBounds = false
+        overlayNode.layer.shadowOffset = CGSize(width: -1.0, height: 0.0)
+        overlayNode.layer.shadowOpacity = 0.20
+        overlayNode.layer.shadowRadius = 3.0
+        overlayNode.layer.shadowPath = UIBezierPath(rect: fromController.view.bounds).cgPath
+        
+        toController.view.frame.origin.x = 0 - (container.frame.width * self.offsetMenuModifier)
+        
+        container.addSubview(toController.view)
+        container.addSubnode(overlayNode)
+        container.addSubview(snapshot)
+        
+        // +1 to remove black line between to and from controllers and position to under from
+        let width: CGFloat = container.frame.width - (container.frame.width * offsetMenuModifier) + 1
+        toController.view.frame.size = CGSize(width: width, height: container.frame.height)
+        
+        fromController.view.isHidden = true
+
+        UIView.animate(withDuration: self.animationDuration, delay: self.animationDelay, options: [.curveEaseOut], animations: {
+            let percentageOfScreen: CGFloat = (container.frame.width * self.offsetMenuModifier)
+            self.snapshot.frame.origin.x = container.frame.width - percentageOfScreen
+            self.overlayNode.frame.origin.x = container.frame.width - percentageOfScreen
+            toController.view.frame.origin.x = 0
+            self.snapshot.alpha = 0.2
+        }) { (success) in
+            completion(success)
+        }
+    }
+    
+    override func dismiss(toController: UIViewController, fromController: UIViewController, container: UIView, completion: @escaping (Bool) -> Void) {
+        UIView.animate(withDuration: self.animationDuration, delay: self.animationDelay, options: [.curveEaseOut], animations: {
+            self.snapshot.frame.origin.x = 0.0
+            self.overlayNode.frame = self.snapshot.frame
+            self.snapshot.alpha = 1.0
+            fromController.view.frame.origin.x = 0 - (container.frame.width * self.offsetMenuModifier)
+        }) { (success) in
+            completion(success)
+        }
+    }
+    
+    override func animationEnded(_ transitionCompleted: Bool) {
+        super.animationEnded(transitionCompleted)
+        if let toViewController = toViewController, transitionCompleted, type == .dismiss {
+            toViewController.view.isHidden = false
+        }
+    }
+    
+    override func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return self
+    }
+    
+    override func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        guard let _ = mainController, let _ = menuController else { return nil }
+        return self
+    }
+    
+    func didTapOverlay() {
+        guard let toViewController = self.toViewController else { return }
+        toViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    func interactionPanHandlerForPresentation(pan: UIPanGestureRecognizer) {
+        guard let mainController = mainController, let menuController = menuController else { return }
+        let translation = pan.translation(in: pan.view!.superview!)
+        var progress = (translation.x / (pan.view!.frame.width * 0.9))
+        progress = CGFloat(fminf(fmaxf(Float(progress), 0.0), 1.0))
+        
+        switch pan.state {
+            
+        case .began:
+            menuController.transitioningDelegate = self
+            mainController.present(menuController, animated: true, completion: nil)
+            
+        case .changed:
+            update(progress)
+            
+        case .cancelled:
+            cancel()
+            
+        case .ended:
+            if progress < 0.3 {
+                cancel()
+            } else if progress > 0.99 {
+                // Because of weird bug when user manually drags to 1.0
+                finish()
+            } else {
+                finish()
+            }
+            
+        default:
+            return
+        }
+    }
+    
+    func interactionPanHandler(pan: UIPanGestureRecognizer) {
+        let translation = pan.translation(in: pan.view!.window!)
+        let translationX = translation.x >= 0 ? translation.x : (translation.x * -1)
+        var progress = (translationX / (UIScreen.main.bounds.width * 0.9))
+        progress = CGFloat(fminf(fmaxf(Float(progress), 0.0), 1.0))
+        
+        switch pan.state {
+            
+        case .began:
+            toViewController?.dismiss(animated: true, completion: nil)
+            
+        case .changed:
+            update(progress)
+            
+        case .cancelled:
+            cancel()
+            
+        case .ended:
+            if progress < 0.3 {
+                cancel()
+            } else if progress > 0.99 {
+                // Because of weird bug when user manually drags to 1.0
+                finish()
+            } else {
+                finish()
+            }
+            
+        default:
+            return
+        }
+    }
+}
