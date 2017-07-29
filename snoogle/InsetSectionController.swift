8f8fb8f430dc8e8fdd1dae535f2b1fcea8b2b80b
Vincent Moore <devdebonair@gmail.com>
2017-07-29 16:20:08 -0400

    added inset section controller

---
+++ snoogle/InsetSectionController.swift
@@ -0,0 +1,24 @@
+//
+//  InsetSectionController.swift
+//  snoogle
+//
+//  Created by Vincent Moore on 7/29/17.
+//  Copyright © 2017 Vincent Moore. All rights reserved.
+//
+
+import Foundation
+import IGListKit
+import AsyncDisplayKit
+
+class InsetSectionController: GenericSectionController {
+    init(inset: UIEdgeInsets = .zero) {
+        super.init()
+        self.inset = inset
+        let bottom = self.inset.bottom
+        self.inset.bottom = 0
+        print(isLastSection)
+        if isLastSection {
+            self.inset.bottom = bottom
+        }
+    }
+}
