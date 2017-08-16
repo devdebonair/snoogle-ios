96d999db2342cd4a8866d3d5e353cad5a5f673dc
Vincent Moore <devdebonair@gmail.com>
2017-08-15 23:21:51 -0400

    increased font size in posts

--- snoogle/PostViewModel.swift
+++ snoogle/PostViewModel.swift
@@ -107,7 +107,7 @@ class PostViewModel: NSObject, ViewModelElement, CellNodePostDelegate {
         let stickyColor = UIColor(colorLiteralRed: 38/255, green: 166/255, blue: 91/255, alpha: 1.0)
         let stickyFont = UIFont.systemFont(ofSize: 15, weight: UIFontWeightHeavy)
         
-        let titleFont = UIFont.systemFont(ofSize: 14, weight: UIFontWeightBold)
+        let titleFont = UIFont.systemFont(ofSize: 16, weight: UIFontWeightRegular)
         let titleLineSpacing: CGFloat = 4.0
         
         let metaFont = UIFont.systemFont(ofSize: 10)
